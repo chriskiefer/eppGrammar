7dc0294a2484d65abac31ceffaddeb6812a7500c
Francisco Bernardo <franxico@gmail.com>
2019-02-02 13:25:56 +0000

    replacing builtins

--- eppgrammar.ne
+++ eppgrammar.ne
@@ -14,17 +14,48 @@ const lexer = moo.compile({
 # Pass your lexer object using the @lexer option:
 @lexer lexer
 
-main -> _ statement _
+main -> _ Statement _
 
-statement ->
-    func %lparen statement %rparen
-  | %number
+Statement ->
+  func %lparen Statement %rparen
+  | Number
 
 func ->
-    %functionname
+  %functionname
   | %functionkeyword
 
 
+
+# Numbers — defining our rules can be more efficient that using builtins
+
+Number -> _number {% function(d) {return {'literal': parseFloat(d[0])}} %}
+
+_posint ->
+  [0-9] {% id %}
+  | _posint [0-9] {% function(d) {return d[0] + d[1]} %}
+
+_int ->
+	"-" _posint {% function(d) {return d[0] + d[1]; }%}
+	| _posint {% id %}
+
+_float ->
+	_int {% id %}
+  | _int "." _posint {% function(d) {return d[0] + d[1] + d[2]; }%}
+  | "." _posint {% function(d) {return "0" + d[0] + d[1]; }%}
+
+_number ->
+  _float {% id %}
+  | _float "e" _int {% function(d){return d[0] + d[1] + d[2]; } %}
+
+
+
+# Whitespace
+
 _  -> wschar:* {% function(d) {return null;} %}
 __ -> wschar:+ {% function(d) {return null;} %}
-wschar -> %ws {% id %}
+
+wschar -> [ \t\n\v\f] {% id %}
+
+
+# _ -> null | _ [\s] {% function() {} %}
+# __ -> [\s] | __ [\s] {% function() {} %}
--- eppprocessor.js
+++ eppprocessor.js
@@ -18,16 +18,26 @@ var grammar = {
     ParserRules: [
     {"name": "main", "symbols": ["_", "statement", "_"]},
     {"name": "statement", "symbols": ["func", (lexer.has("lparen") ? {type: "lparen"} : lparen), "statement", (lexer.has("rparen") ? {type: "rparen"} : rparen)]},
-    {"name": "statement", "symbols": [(lexer.has("number") ? {type: "number"} : number)]},
+    {"name": "statement", "symbols": ["Number"]},
     {"name": "func", "symbols": [(lexer.has("functionname") ? {type: "functionname"} : functionname)]},
     {"name": "func", "symbols": [(lexer.has("functionkeyword") ? {type: "functionkeyword"} : functionkeyword)]},
+    {"name": "Number", "symbols": ["_number"], "postprocess": function(d) {return {'literal': parseFloat(d[0])}}},
+    {"name": "_posint", "symbols": [/[0-9]/], "postprocess": id},
+    {"name": "_posint", "symbols": ["_posint", /[0-9]/], "postprocess": function(d) {return d[0] + d[1]}},
+    {"name": "_int", "symbols": [{"literal":"-"}, "_posint"], "postprocess": function(d) {return d[0] + d[1]; }},
+    {"name": "_int", "symbols": ["_posint"], "postprocess": id},
+    {"name": "_float", "symbols": ["_int"], "postprocess": id},
+    {"name": "_float", "symbols": ["_int", {"literal":"."}, "_posint"], "postprocess": function(d) {return d[0] + d[1] + d[2]; }},
+    {"name": "_float", "symbols": [{"literal":"."}, "_posint"], "postprocess": function(d) {return "0" + d[0] + d[1]; }},
+    {"name": "_number", "symbols": ["_float"], "postprocess": id},
+    {"name": "_number", "symbols": ["_float", {"literal":"e"}, "_int"], "postprocess": function(d){return d[0] + d[1] + d[2]; }},
     {"name": "_$ebnf$1", "symbols": []},
     {"name": "_$ebnf$1", "symbols": ["_$ebnf$1", "wschar"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "_", "symbols": ["_$ebnf$1"], "postprocess": function(d) {return null;}},
     {"name": "__$ebnf$1", "symbols": ["wschar"]},
     {"name": "__$ebnf$1", "symbols": ["__$ebnf$1", "wschar"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "__", "symbols": ["__$ebnf$1"], "postprocess": function(d) {return null;}},
-    {"name": "wschar", "symbols": [(lexer.has("ws") ? {type: "ws"} : ws)], "postprocess": id}
+    {"name": "wschar", "symbols": [/[ \t\n\v\f]/], "postprocess": id}
 ]
   , ParserStart: "main"
 }
