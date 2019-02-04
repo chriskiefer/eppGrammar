b1ebc47ff5fcd7e6ba1b2759925c3077e78f7545
Francisco Bernardo <franxico@gmail.com>
2019-02-04 09:56:54 +0000

    removed the use of builtins for efficiency now using Moo as lexer

--- eppgrammar.ne
+++ eppgrammar.ne
@@ -1,61 +1,67 @@
+# Builtins make your parser slower. For efficiency, use lexer like Moo
 @{%
 const moo = require("moo"); // this 'require' creates a node dependency
 
 const lexer = moo.compile({
-  functionkeyword: ['sinosc','add'],
-  functionname: /[a-zA-Z][a-zA-Z0-9]*/,
-  number: /[-+]?[0-9]*\.[0-9]+/,
-  ws:     /[ \t\v\f]/,
+  functionkeyword: ['sinosc', 'phasor', 'adsr', 'filter', 'samp'],
+  functionname: /[a-zA-Z][a-zA-Z0-9]*?/,
+  number: /[-+]?[0-9]*?\.[0-9]+?/,
+  ws: {match: /\s+/, lineBreaks: true},
   lparen: /\(/,
   rparen: /\)/,
+  mult: /\*/,
+  add: /\+/,
+  dot: /\./
 });
 %}
 
-# Pass your lexer object using the @lexer option:
+# Pass your lexer object using the @lexer option
 @lexer lexer
 
 main -> _ Statement _
 
 Statement ->
   func %lparen Statement %rparen
-  | Number
+  | Statement %dot Statement
+  | Statement %add Statement
+  | Statement %mult Statement
+  | %number
 
 func ->
   %functionname
   | %functionkeyword
 
+  # Whitespace
 
+  _  -> wschar:* {% function(d) {return null;} %}
+  __ -> wschar:+ {% function(d) {return null;} %}
 
-# Numbers —  builtins make your parser slower. for efficiency, you would want to switch to a lexer
-
-
-Number -> _number {% function(d) {return {'literal': parseFloat(d[0])}} %}
-
-_posint ->
-  [0-9] {% id %}
-  | _posint [0-9] {% function(d) {return d[0] + d[1]} %}
-
-_int ->
-	"-" _posint {% function(d) {return d[0] + d[1]; }%}
-	| _posint {% id %}
-
-_float ->
-	_int {% id %}
-  | _int "." _posint {% function(d) {return d[0] + d[1] + d[2]; }%}
-  | "." _posint {% function(d) {return "0" + d[0] + d[1]; }%}
-
-_number ->
-  _float {% id %}
-  | _float "e" _int {% function(d){return d[0] + d[1] + d[2]; } %}
+  wschar -> %ws {% id %}
 
 
 
-# Whitespace
+# Number -> _number {% function(d) {return {'literal': parseFloat(d[0])}} %}
+#
+# _posint ->
+#   [0-9] {% id %}
+#   | _posint [0-9] {% function(d) {return d[0] + d[1]} %}
+#
+# _int ->
+# 	"-" _posint {% function(d) {return d[0] + d[1]; }%}
+# 	| _posint {% id %}
+#
+# _float ->
+# 	_int {% id %}
+#   | _int "." _posint {% function(d) {return d[0] + d[1] + d[2]; }%}
+#   | "." _posint {% function(d) {return "0" + d[0] + d[1]; }%}
+#
+# _number ->
+#   _float {% id %}
+#   | _float "e" _int {% function(d){return d[0] + d[1] + d[2]; } %}
 
-_  -> wschar:* {% function(d) {return null;} %}
-__ -> wschar:+ {% function(d) {return null;} %}
 
-wschar -> [ \t\n\v\f] {% id %}
+
+
 
 
 # _ -> null | _ [\s] {% function() {} %}
--- eppprocessor.js
+++ eppprocessor.js
@@ -6,38 +6,34 @@ function id(x) { return x[0]; }
 const moo = require("moo"); // this 'require' creates a node dependency
 
 const lexer = moo.compile({
-  functionkeyword: ['sinosc','add'],
-  functionname: /[a-zA-Z][a-zA-Z0-9]*/,
-  number: /[-+]?[0-9]*\.[0-9]+/,
-  ws:     /[ \t\v\f]/,
+  functionkeyword: ['sinosc', 'phasor', 'adsr', 'filter', 'samp'],
+  functionname: /[a-zA-Z][a-zA-Z0-9]*?/,
+  number: /[-+]?[0-9]*?\.[0-9]+?/,
+  ws: {match: /\s+/, lineBreaks: true},
   lparen: /\(/,
   rparen: /\)/,
+  mult: /\*/,
+  add: /\+/,
+  dot: /\./
 });
 var grammar = {
     Lexer: lexer,
     ParserRules: [
-    {"name": "main", "symbols": ["_", "statement", "_"]},
-    {"name": "statement", "symbols": ["func", (lexer.has("lparen") ? {type: "lparen"} : lparen), "statement", (lexer.has("rparen") ? {type: "rparen"} : rparen)]},
-    {"name": "statement", "symbols": ["Number"]},
+    {"name": "main", "symbols": ["_", "Statement", "_"]},
+    {"name": "Statement", "symbols": ["func", (lexer.has("lparen") ? {type: "lparen"} : lparen), "Statement", (lexer.has("rparen") ? {type: "rparen"} : rparen)]},
+    {"name": "Statement", "symbols": ["Statement", (lexer.has("dot") ? {type: "dot"} : dot), "Statement"]},
+    {"name": "Statement", "symbols": ["Statement", (lexer.has("add") ? {type: "add"} : add), "Statement"]},
+    {"name": "Statement", "symbols": ["Statement", (lexer.has("mult") ? {type: "mult"} : mult), "Statement"]},
+    {"name": "Statement", "symbols": [(lexer.has("number") ? {type: "number"} : number)]},
     {"name": "func", "symbols": [(lexer.has("functionname") ? {type: "functionname"} : functionname)]},
     {"name": "func", "symbols": [(lexer.has("functionkeyword") ? {type: "functionkeyword"} : functionkeyword)]},
-    {"name": "Number", "symbols": ["_number"], "postprocess": function(d) {return {'literal': parseFloat(d[0])}}},
-    {"name": "_posint", "symbols": [/[0-9]/], "postprocess": id},
-    {"name": "_posint", "symbols": ["_posint", /[0-9]/], "postprocess": function(d) {return d[0] + d[1]}},
-    {"name": "_int", "symbols": [{"literal":"-"}, "_posint"], "postprocess": function(d) {return d[0] + d[1]; }},
-    {"name": "_int", "symbols": ["_posint"], "postprocess": id},
-    {"name": "_float", "symbols": ["_int"], "postprocess": id},
-    {"name": "_float", "symbols": ["_int", {"literal":"."}, "_posint"], "postprocess": function(d) {return d[0] + d[1] + d[2]; }},
-    {"name": "_float", "symbols": [{"literal":"."}, "_posint"], "postprocess": function(d) {return "0" + d[0] + d[1]; }},
-    {"name": "_number", "symbols": ["_float"], "postprocess": id},
-    {"name": "_number", "symbols": ["_float", {"literal":"e"}, "_int"], "postprocess": function(d){return d[0] + d[1] + d[2]; }},
     {"name": "_$ebnf$1", "symbols": []},
     {"name": "_$ebnf$1", "symbols": ["_$ebnf$1", "wschar"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "_", "symbols": ["_$ebnf$1"], "postprocess": function(d) {return null;}},
     {"name": "__$ebnf$1", "symbols": ["wschar"]},
     {"name": "__$ebnf$1", "symbols": ["__$ebnf$1", "wschar"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "__", "symbols": ["__$ebnf$1"], "postprocess": function(d) {return null;}},
-    {"name": "wschar", "symbols": [/[ \t\n\v\f]/], "postprocess": id}
+    {"name": "wschar", "symbols": [(lexer.has("ws") ? {type: "ws"} : ws)], "postprocess": id}
 ]
   , ParserStart: "main"
 }
