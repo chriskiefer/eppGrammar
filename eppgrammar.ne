5aef5895f555b68001d1b25b947f0668b5575273
Francisco Bernardo <franxico@gmail.com>
2019-02-13 15:31:22 +0000

    on epp target language coming up

--- eppgrammar.ne
+++ eppgrammar.ne
@@ -4,65 +4,105 @@ const moo = require("moo"); // this 'require' creates a node dependency
 
 const lexer = moo.compile({
   functionkeyword: ['sinosc', 'phasor', 'adsr', 'filter', 'samp'],
-  functionname: /[a-zA-Z][a-zA-Z0-9]*?/,
-  number: /[-+]?[0-9]*?\.[0-9]+?/,
+  o: /o/,
+  x: /x/,
+  functionname: /[a-zA-Z][a-zA-Z0-9]*/,
+  number: /[-+]?[0-9]*\.?[0-9]+/,
   ws: {match: /\s+/, lineBreaks: true},
   lparen: /\(/,
   rparen: /\)/,
+  lbrack: /\[/,
+  rbrack: /\]/,
+  pipe: /\|/,
   mult: /\*/,
   add: /\+/,
-  dot: /\./
+  dot: /\./,
+  assign: /->/,
+  bindr: />>/,
+  bindl: /<</,
+  ampmore: /\(\(/,
+  ampless: /\)\)/,
+  silence: /!/,
+  transpmore: /\+/,
+  underscore: /\_/,
+  hyphen: /\-/,
+  ndash: /\–/,
+  mdash: /\—/,
+  colon: /\:/,
+  semicolon: /\;/
 });
 %}
 
 # Pass your lexer object using the @lexer option
 @lexer lexer
 
-main -> _ Statement _
+main -> Lines {% d =>  ({ "@lang" : d[0] })  %}
 
-Statement ->
-  func %lparen Statement %rparen
-  | Statement %dot Statement
-  | Statement %add Statement
-  | Statement %mult Statement
-  | %number
 
-func ->
-  %functionname
-  | %functionkeyword
+Lines ->
+      Line _ %semicolon _ Lines {% d => [{ "@spawn" : d[0] }].concat(d[4]) %}
+      | Line  {% d => [{ "@spawn" : d[0] }] %}
 
-  # Whitespace
+# Line ->
+#     %functionname {% id %}
+#     | %functionkeyword {% id %}
 
-  _  -> wschar:* {% function(d) {return null;} %}
-  __ -> wschar:+ {% function(d) {return null;} %}
 
-  wschar -> %ws {% id %}
+Line ->
+       Synth {% id %}
+      | Loop  {% id %}
+      | Beats {% id %}
 
 
 
-# Number -> _number {% function(d) {return {'literal': parseFloat(d[0])}} %}
-#
-# _posint ->
-#   [0-9] {% id %}
-#   | _posint [0-9] {% function(d) {return d[0] + d[1]} %}
-#
-# _int ->
-# 	"-" _posint {% function(d) {return d[0] + d[1]; }%}
-# 	| _posint {% id %}
-#
-# _float ->
-# 	_int {% id %}
-#   | _int "." _posint {% function(d) {return d[0] + d[1] + d[2]; }%}
-#   | "." _posint {% function(d) {return "0" + d[0] + d[1]; }%}
-#
-# _number ->
-#   _float {% id %}
-#   | _float "e" _int {% function(d){return d[0] + d[1] + d[2]; } %}
+Synth -> Params _ %bindr _ Functions  {% d => ({
+                                                "@synth": {
+                                                  "@params": d[0],
+                                                  "@functions": d[4]
+                                                }
+                                              })
+                                      %}
+
+Params ->
+        %number _ %pipe _ Params  {% d =>  [ parseFloat(d[0]), d[4] ] %}
+        | %number {% d => parseFloat(d[0]) %}
+
+Functions ->
+            %functionkeyword _ %bindr _ Functions {% d => [d[0]].concat(d[4]) %}
+            | %functionkeyword                 {% id %}
+
+Loop -> "[" Beats "]" {%
+                      function(d) {
+                        return { "@loop": d[1] };
+                      }
+                      %}
+
+Beats -> Beat:+ {%
+                function(d) {
+                  return { "@beats": d[0].join() };
+                }
+                %}
+
+Beat ->
+    Rest          {% id %}
+    | Hat         {% id %}
+    | Snare       {% id %}
+    | Kick        {% id %}
+
+Rest -> %dot      {% id %}
+
+Hat -> %hyphen    {% id %}
+
+Snare -> %o       {% id %}
+
+Kick -> %x        {% id %}
 
 
 
 
+# Whitespace
 
+_  -> wschar:* {% function(d) {return null;} %}
+__ -> wschar:+ {% function(d) {return null;} %}
 
-# _ -> null | _ [\s] {% function() {} %}
-# __ -> [\s] | __ [\s] {% function() {} %}
+wschar -> %ws {% id %}
--- eppprocessor.js
+++ eppprocessor.js
@@ -7,26 +7,73 @@ const moo = require("moo"); // this 'require' creates a node dependency
 
 const lexer = moo.compile({
   functionkeyword: ['sinosc', 'phasor', 'adsr', 'filter', 'samp'],
-  functionname: /[a-zA-Z][a-zA-Z0-9]*?/,
-  number: /[-+]?[0-9]*?\.[0-9]+?/,
+  o: /o/,
+  x: /x/,
+  functionname: /[a-zA-Z][a-zA-Z0-9]*/,
+  number: /[-+]?[0-9]*\.?[0-9]+/,
   ws: {match: /\s+/, lineBreaks: true},
   lparen: /\(/,
   rparen: /\)/,
+  lbrack: /\[/,
+  rbrack: /\]/,
+  pipe: /\|/,
   mult: /\*/,
   add: /\+/,
-  dot: /\./
+  dot: /\./,
+  assign: /->/,
+  bindr: />>/,
+  bindl: /<</,
+  ampmore: /\(\(/,
+  ampless: /\)\)/,
+  silence: /!/,
+  transpmore: /\+/,
+  underscore: /\_/,
+  hyphen: /\-/,
+  ndash: /\–/,
+  mdash: /\—/,
+  colon: /\:/,
+  semicolon: /\;/
 });
 var grammar = {
     Lexer: lexer,
     ParserRules: [
-    {"name": "main", "symbols": ["_", "Statement", "_"]},
-    {"name": "Statement", "symbols": ["func", (lexer.has("lparen") ? {type: "lparen"} : lparen), "Statement", (lexer.has("rparen") ? {type: "rparen"} : rparen)]},
-    {"name": "Statement", "symbols": ["Statement", (lexer.has("dot") ? {type: "dot"} : dot), "Statement"]},
-    {"name": "Statement", "symbols": ["Statement", (lexer.has("add") ? {type: "add"} : add), "Statement"]},
-    {"name": "Statement", "symbols": ["Statement", (lexer.has("mult") ? {type: "mult"} : mult), "Statement"]},
-    {"name": "Statement", "symbols": [(lexer.has("number") ? {type: "number"} : number)]},
-    {"name": "func", "symbols": [(lexer.has("functionname") ? {type: "functionname"} : functionname)]},
-    {"name": "func", "symbols": [(lexer.has("functionkeyword") ? {type: "functionkeyword"} : functionkeyword)]},
+    {"name": "main", "symbols": ["Lines"], "postprocess": d =>  ({ "@lang" : d[0] })},
+    {"name": "Lines", "symbols": ["Line", "_", (lexer.has("semicolon") ? {type: "semicolon"} : semicolon), "_", "Lines"], "postprocess": d => [{ "@spawn" : d[0] }].concat(d[4])},
+    {"name": "Lines", "symbols": ["Line"], "postprocess": d => [{ "@spawn" : d[0] }]},
+    {"name": "Line", "symbols": ["Synth"], "postprocess": id},
+    {"name": "Line", "symbols": ["Loop"], "postprocess": id},
+    {"name": "Line", "symbols": ["Beats"], "postprocess": id},
+    {"name": "Synth", "symbols": ["Params", "_", (lexer.has("bindr") ? {type: "bindr"} : bindr), "_", "Functions"], "postprocess":  d => ({
+          "@synth": {
+            "@params": d[0],
+            "@functions": d[4]
+          }
+        })
+                                              },
+    {"name": "Params", "symbols": [(lexer.has("number") ? {type: "number"} : number), "_", (lexer.has("pipe") ? {type: "pipe"} : pipe), "_", "Params"], "postprocess": d =>  [ parseFloat(d[0]), d[4] ]},
+    {"name": "Params", "symbols": [(lexer.has("number") ? {type: "number"} : number)], "postprocess": d => parseFloat(d[0])},
+    {"name": "Functions", "symbols": [(lexer.has("functionkeyword") ? {type: "functionkeyword"} : functionkeyword), "_", (lexer.has("bindr") ? {type: "bindr"} : bindr), "_", "Functions"], "postprocess": d => [d[0]].concat(d[4])},
+    {"name": "Functions", "symbols": [(lexer.has("functionkeyword") ? {type: "functionkeyword"} : functionkeyword)], "postprocess": id},
+    {"name": "Loop", "symbols": [{"literal":"["}, "Beats", {"literal":"]"}], "postprocess": 
+        function(d) {
+          return { "@loop": d[1] };
+        }
+        },
+    {"name": "Beats$ebnf$1", "symbols": ["Beat"]},
+    {"name": "Beats$ebnf$1", "symbols": ["Beats$ebnf$1", "Beat"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "Beats", "symbols": ["Beats$ebnf$1"], "postprocess": 
+        function(d) {
+          return { "@beats": d[0].join() };
+        }
+        },
+    {"name": "Beat", "symbols": ["Rest"], "postprocess": id},
+    {"name": "Beat", "symbols": ["Hat"], "postprocess": id},
+    {"name": "Beat", "symbols": ["Snare"], "postprocess": id},
+    {"name": "Beat", "symbols": ["Kick"], "postprocess": id},
+    {"name": "Rest", "symbols": [(lexer.has("dot") ? {type: "dot"} : dot)], "postprocess": id},
+    {"name": "Hat", "symbols": [(lexer.has("hyphen") ? {type: "hyphen"} : hyphen)], "postprocess": id},
+    {"name": "Snare", "symbols": [(lexer.has("o") ? {type: "o"} : o)], "postprocess": id},
+    {"name": "Kick", "symbols": [(lexer.has("x") ? {type: "x"} : x)], "postprocess": id},
     {"name": "_$ebnf$1", "symbols": []},
     {"name": "_$ebnf$1", "symbols": ["_$ebnf$1", "wschar"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "_", "symbols": ["_$ebnf$1"], "postprocess": function(d) {return null;}},
